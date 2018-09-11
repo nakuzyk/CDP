778502d2c05b075574a5bb602f2f7768cd527062
Anastasiia Kuzyk <anastasiia_kuzyk@epam.com>
2018-09-11 16:59:24 +0300

    Task 2 done

--- Kuzyk/Task2/src/com/epam/anastasiia_kuzyk/java/lesson2/task1/GiftBox/Gift.java
+++ Kuzyk/Task2/src/com/epam/anastasiia_kuzyk/java/lesson2/task1/GiftBox/Gift.java
@@ -1,48 +1,56 @@
 package com.epam.anastasiia_kuzyk.java.lesson2.task1.GiftBox;
 
-import java.util.Arrays;
 import java.util.Scanner;
+
 import com.epam.anastasiia_kuzyk.java.lesson2.task1.Candies.Cookies;
 import com.epam.anastasiia_kuzyk.java.lesson2.task1.Candies.Confection;
 import com.epam.anastasiia_kuzyk.java.lesson2.task1.Candies.Marshmallow;
 import com.epam.anastasiia_kuzyk.java.lesson2.task1.Candies.Chocolate;
-import com.epam.anastasiia_kuzyk.java.lesson2.task1.Computation.SortByCost;
-import com.epam.anastasiia_kuzyk.java.lesson2.task1.Computation.SortDescByCost;
+
 
 public class Gift {
 
-    public static void createBox() {
-        Gift gift = new Gift();
-        Confection[] list = gift.createArray();
-        System.out.println("Creating a collection");
+    private static Integer iterator = 0;
+    private static Integer volume = 6;
+    private Confection[] list = new Confection[volume];
 
-        gift.displayArrays(list);
-
-        double res = gift.getFinalWeight(list, 0);
-        System.out.println("\n2. Total weight of all items is: " + res + "\n");
-        System.out.println("Final weight is calculated: " + res);
-
-        System.out.println("3. Sorted list by Cost:\n");
-        Arrays.sort(list, new SortByCost());
-        gift.displayArrays(list);
-        System.out.println();
-        Arrays.sort(list, new SortDescByCost());
-        gift.displayArrays(list);
-        System.out.println("Sorting was successful");
-
-        showTheLastInfoMessage();
-        gift.findElementByCost(list);
-        System.out.println("No errors during searching for candy");
+    public void addConfection(Confection confection) {
+        if (iterator < volume - 1)
+            list[iterator++] = confection;
+        else {
+            doubleList();
+            list[iterator++] = confection;
+        }
     }
+
+    public void removeConfection(int i) {
+        if (iterator < i) {
+            return;
+        }
+        for(;i < iterator; i++){
+            list[i] = list[i+1];
+        }
+        list[iterator--] = null;
+    }
+
+    public void doubleList(){
+        volume*=2;
+        Confection[] newList =  new Confection[volume];
+        System.arraycopy(list, 0, newList, 0, iterator);
+        setList(newList);
+    }
+
 
     /**
      * First method. Returns the total cost of all items from list (ArrayList
      * <Confection> list)
      */
 
-    public double getFinalWeight(Confection[] list, double sum) {
+    public double getFinalWeight(double sum) {
         for (Confection x : list) {
-            sum += x.getWeight();
+            if (x != null ) {
+                sum += x.getWeight();
+            }
         }
         return sum;
     }
@@ -53,20 +61,22 @@ public class Gift {
      * user types in console.
      */
 
-    public void findElementByCost(Confection[] list) {
+    public void findElementByCost() {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         sc.close();
-        StringBuilder item = findCandyByCost(a, b, list);
+        StringBuilder item = findCandyByCost(a, b);
         System.out.println("\nYour candies are: " + item);
     }
 
-    public StringBuilder findCandyByCost(double a, double b, Confection[] list) {
+    public StringBuilder findCandyByCost(double a, double b) {
         StringBuilder candy = new StringBuilder();
         for (Confection x : list) {
-            if ((x.getCost() >= a && x.getCost() <= b) || (x.getCost() >= b && x.getCost() <= a)) {
-                candy.append(x.getName()).append("(").append(x.getType()).append("), ");
+            if (x != null ) {
+                if ((x.getCost() >= a && x.getCost() <= b) || (x.getCost() >= b && x.getCost() <= a)) {
+                    candy.append(x.getName()).append("(").append(x.getType()).append("), ");
+                }
             }
         }
         if (a < 0 || b < 0) {
@@ -81,33 +91,41 @@ public class Gift {
      * parameters.
      */
 
-    public void displayArrays(Confection[] list) {
+    public void displayArrays() {
         int i = 1;
         //System.out.printf("ID: " + " NAME:    " + "   COST: " + "     WEIGHT:  " + "TYPE:");
         System.out.println("ID: " + " NAME:    " + "   COST: " + "     WEIGHT:  " + "   TYPE:");
         for (Confection x : list) {
-            System.out.println(i + ".   " + x.getName() + "     " + x.getCost() + "        " + x.getWeight()
-                        + "      " + x.getType());
-            i++;
+           if (x != null ) {
+               System.out.println(i + ".   " + x.getName() + "     " + x.getCost() + "        " + x.getWeight()
+                       + "      " + x.getType());
+               i++;
+           }
         }
     }
 
-    public Confection[] createArray() {
-        Confection[] list = new Confection[6];
+    public void createArray() {
+        addConfection(new Chocolate("Snikers ", 2.99, 50, "Chocolate", "Hazelnut"));
+        addConfection(new Chocolate("Mars    ", 2.51, 45, "Chocolate", "Meadows"));
+        addConfection(new Marshmallow("Zephyr   ", 6.10, 80.1, "Marshmallow", "Blueberry"));
+        addConfection(new Marshmallow("American ", 8.50, 95.5, "Marshmallow", "Strawberry"));
+        addConfection(new Cookies("Sand    ", 1.99, 30, "Cookie", 5));
+        addConfection(new Cookies("Cracker ", 3.99, 50.5, "Cookie", 10));
+    }
 
-        list[0] = new Chocolate("Snikers ", 2.99, 50, "Chocolate", "Hazelnut");
-        list[1] = new Chocolate("Mars    ", 2.51, 45, "Chocolate", "Meadows");
-        list[2] = new Marshmallow("Zephyr   ", 6.10, 80.1, "Marshmallow", "Blueberry");
-        list[3] = new Marshmallow("American ", 8.50, 95.5, "Marshmallow", "Strawberry");
-        list[4] = new Cookies("Sand    ", 1.99, 30, "Cookie", 5);
-        list[5] = new Cookies("Cracker ", 3.99, 50.5, "Cookie", 10);
-
+    public Confection[] getList() {
         return list;
     }
 
-    /** Text messages */
+    public void setList(Confection[] list) {
+        this.list = list;
+    }
 
-    public static void showTheLastInfoMessage(){
+    /**
+     * Text messages
+     */
+
+    public static void showTheLastInfoMessage() {
         System.out.println(
                 "\nAnd now let's find some candies from gift-box by thier cost.\nPlease enter first and last numbers of range");
     }
--- Kuzyk/Task2/src/com/epam/anastasiia_kuzyk/java/lesson2/task1/GiftBox/GiftBox.java
+++ Kuzyk/Task2/src/com/epam/anastasiia_kuzyk/java/lesson2/task1/GiftBox/GiftBox.java
@@ -4,7 +4,10 @@ import com.epam.anastasiia_kuzyk.java.lesson2.task1.Candies.Confection;
 import  java.util.ArrayList;
 
 public interface GiftBox {
-    public double getFinalCost(ArrayList<Confection> list, double sum);
-    public void findElementByCost(ArrayList<Confection> list);
-    public void displayCollection(ArrayList<Confection> list);
+    public double getFinalCost(double sum);
+    public void findElementByCost();
+    public void displayArrays();
+//    public double getFinalCost(ArrayList<Confection> list, double sum);
+//    public void findElementByCost(ArrayList<Confection> list);
+//    public void displayCollection(ArrayList<Confection> list);
 }
